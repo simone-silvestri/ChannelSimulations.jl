ed05a5805ce0585881923543f64cd97da3ed1f28
Simone Silvestri <silvestri.simone0@gmail.com>
2024-09-06 10:46:51 -0400

    fix move to mat

--- visualize/move_to_mat.jl
+++ visualize/move_to_mat.jl
@@ -5,12 +5,12 @@ function move_to_mat(mom, tra; prefix = "averages")
     file = jldopen("channel_$(prefix)_$(mom * 10 + tra).jld2", "r")
 
     iter = keys(file["timeseries/t"])[end]
-    Pu = file["timeseries/Pv/$iter"]
-    Pv = file["timeseries/Pu/$iter"]
-    Pw = file["timeseries/Pu/$iter"]
-    Bx = file["timeseries/Pu/$iter"]
-    By = file["timeseries/Pu/$iter"]
-    Bz = file["timeseries/Pu/$iter"]
+    Pu = file["timeseries/Pu/$iter"]
+    Pv = file["timeseries/Pv/$iter"]
+    Pw = file["timeseries/Pw/$iter"]
+    Bx = file["timeseries/∂xb²/$iter"]
+    By = file["timeseries/∂yb²/$iter"]
+    Bz = file["timeseries/∂zb²/$iter"]
 
     U = file["timeseries/Uⁿ⁻¹/$iter"]
     V = file["timeseries/Vⁿ⁻¹/$iter"]
